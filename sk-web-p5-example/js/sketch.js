eb006b2f7dedd6fbb493342c30e9c18e212b9949
Kevin Vennitti <kevin.vennitti@gmail.com>
2020-04-10 09:47:51 +0200

    Hello world

---
+++ sk-web-p5-example/js/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
+}
+
+function draw() {
+  background(20);
+
+  fill(255);
+  rect(50,50,100,100);
+
+  fill(255,0,0);
+  ellipse(250,100,100,100);
+}
